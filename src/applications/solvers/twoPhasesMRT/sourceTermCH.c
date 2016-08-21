0dfde6687c0ce5231c2a5ce2d2fe191aa9901cbd
Ezequiel Fogliatto <ezefogliatto@gmail.com>
2016-08-21 19:40:31 -0300

    T\'ermino de fuente para cahnHilliard

--- src/applications/solvers/twoPhasesMRT/sourceTermCH.c
+++ src/applications/solvers/twoPhasesMRT/sourceTermCH.c
@@ -4,7 +4,7 @@
 
 double* latticeWeights(const struct lattice lat);
 int** latticeVel(const struct lattice lat);
-
+double** sourceMatrix(const struct lattice lat, const struct fields fld);
 
 double* sourceTermCH(const double** U, const double** Uold, const double* phi, const double* phiOld, const struct lattice lat, const struct fields fld, const double dt, const int id) {
 
@@ -21,7 +21,7 @@ double* sourceTermCH(const double** U, const double** Uold, const double* phi, c
     const double ddt_y = (U[id][1] * phi[id]   -   Uold[id][1] * phiOld[id]) / dt;
     const double ddt_z = (U[id][2] * phi[id]   -   Uold[id][2] * phiOld[id]) / dt;    
     
-    unsigned int k;
+    unsigned int i,j,k;
     for(k = 0 ; k < lat.Q ; k++) {
 
 	const double dot = lvel[k][0] * ddt_x  +  lvel[k][1] * ddt_y  +  lvel[k][2] * ddt_z;
@@ -31,9 +31,21 @@ double* sourceTermCH(const double** U, const double** Uold, const double* phi, c
     }
 
 
-    /* double* st2; */
-    /* st2 = (double*)malloc( lat.Q * sizeof(double) ); */
+    double* st2;
+    st2 = (double*)malloc( lat.Q * sizeof(double) );
+    for(k = 0 ; k < lat.Q ; k++) {
+	st2[k] = 0;
+    }
+
+    double** R;
+    R = sourceMatrix(lat,fld);
+
+    for(i = 0 ; i < lat.Q ; i++) {
+	for(j = 0 ; j < lat.Q ; j++) {
+	    st2[i] += R[i][j] * st[i];
+	}
+    }
     
-    return st;
+    return st2;
     
 }
