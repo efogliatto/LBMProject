b463e4fdf195d3cbf438d4e081b952cf3f63ece5
Ezequiel-laptop <ezefogliatto@gmail.com>
2016-08-24 19:56:43 -0300

    liangTotalForce

---
+++ src/lbe/liangTotalForce.h
@@ -0,0 +1,9 @@
+#ifndef LIANGTOTALFORCE_H
+#define LIANGTOTALFORCE_H
+
+#include <basicSolverInfo.h>
+#include <twoPhasesFields.h>
+
+void liangtOTALfORCE(struct twoPhasesFields* fields, const struct solverInfo* info, double* eq, const unsigned int id);
+
+#endif // LIANGTOTALFORCE_H
