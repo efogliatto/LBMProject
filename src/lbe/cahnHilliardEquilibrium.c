f056fa30e0422ca4b925ade45594aefd798d0ac5
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2016-08-27 21:22:33 -0300

    Commit intermedio

--- src/lbe/cahnHilliardEquilibrium.c
+++ src/lbe/cahnHilliardEquilibrium.c
@@ -10,7 +10,7 @@ void cahnHilliardEquilibrium(struct twoPhasesFields* fields, const struct solver
     
     for( k = 1 ; k < info->lattice.Q ; k++) {
     	dot = info->lattice.vel[k][0] * fields->U[id][0]   +   info->lattice.vel[k][1] * fields->U[id][1]   +   info->lattice.vel[k][2] * fields->U[id][2];
-    	eq[k] = info->lattice.omega[k] * info->fields.eta * fields->muPhi[id]   +   info->lattice.c * dot * info->lattice.omega[k] * fields->phi[id] / info->lattice.cs2;
+    	eq[k] = info->lattice.omega[k] * info->fields.eta * fields->muPhi[id]  +   info->lattice.c * dot ;//* info->lattice.omega[k] * fields->phi[id] / info->lattice.cs2;
     }
 
 
