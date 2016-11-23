9cf53a8fa7873a1818b578a6332ce919f0e94c49
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2016-11-23 12:03:22 -0300

    Est\'a funcionando la cavidad boyante. Incorporaci\'on del t\'ermino correctivo en la colisi\'on de g.
    Hay que incorporar la condici\'on de borde de gradiente nulo

--- src/applications/solvers/pseudoPotHeatT/pseudoPotHeatT.c
+++ src/applications/solvers/pseudoPotHeatT/pseudoPotHeatT.c
@@ -149,8 +149,8 @@ int main( int argc, char **argv ) {
     while( updateTime(&info.time) ) {
 	
 	
-    	/* // Collide f (Navier-Stokes) */
-    	/* pseudoPotCollision( &info, mfields.T, mfields.rho, mfields.U, nb, f ); */
+    	// Collide f (Navier-Stokes)
+    	pseudoPotCollision( &info, mfields.T, mfields.rho, mfields.U, nb, f );
 
     	// Density
     	liDensity( &info, mfields.rho, f );
@@ -161,47 +161,47 @@ int main( int argc, char **argv ) {
 
 	
 	
-    	/* // Collide g (Temperature) */
-    	/* temperatureCollision( &info, mfields.T, mfields.rho, mfields.U, nb, g ); */
+    	// Collide g (Temperature)
+    	temperatureCollision( &info, mfields.T, mfields.rho, mfields.U, nb, g );
 	
 	
-    	/* // Stream */
-	/* lbstream( f, f_swp, nb, &info.lattice, &info.parallel ); */
-	/* lbstream( g, g_swp, nb, &info.lattice, &info.parallel ); */
+    	// Stream
+	lbstream( f, f_swp, nb, &info.lattice, &info.parallel );
+	lbstream( g, g_swp, nb, &info.lattice, &info.parallel );
 	
 	
 	
-    	/* // Update macroscopic fields */
+    	// Update macroscopic fields
 
-    	/* // Density */
-    	/* liDensity( &info, mfields.rho, f ); */
+    	// Density
+    	liDensity( &info, mfields.rho, f );
 	
-    	/* // Velocity */
-    	/* pseudoPotVelocity( &info, mfields.rho, mfields.U, f, nb, mfields.T  ); */
+    	// Velocity
+    	pseudoPotVelocity( &info, mfields.rho, mfields.U, f, nb, mfields.T  );
 
-    	/* // Temperature */
-    	/* pseudoPotTemperature( &info, &mfields, g ); */
-
-
-
-
-	/* // Apply boundary conditions */
-	/* updateBC( &bdElements, nb, f, "f", &info.lattice, &mfields ); */
-	/* updateBC( &bdElements, nb, g, "g", &info.lattice, &mfields ); */
-
-	/* // Update macroscopic fields only at boundary */
-	/* updateBoundaryDens( &bdElements, f, &info.lattice, &mfields ); */
-	/* updateBoundaryVel( &info, &bdElements, f, &info.lattice, &mfields, nb ); */
-	/* updateBoundaryT( &bdElements, g, &info.lattice, &mfields ); */
+    	// Temperature
+    	pseudoPotTemperature( &info, &mfields, g );
 
 
 
-	/* // Sync fields */
-	/* syncScalarField(&info.parallel, mfields.rho ); */
-	/* syncScalarField(&info.parallel, mfields.T ); */
-	/* syncPdfField(&info.parallel, mfields.U, 3 ); */
-	/* syncPdfField(&info.parallel, f, info.lattice.Q ); */
-	/* syncPdfField(&info.parallel, g, info.lattice.Q ); */
+
+	// Apply boundary conditions
+	updateBC( &bdElements, nb, f, "f", &info.lattice, &mfields );
+	updateBC( &bdElements, nb, g, "g", &info.lattice, &mfields );
+
+	// Update macroscopic fields only at boundary
+	updateBoundaryDens( &bdElements, f, &info.lattice, &mfields );
+	updateBoundaryVel( &info, &bdElements, f, &info.lattice, &mfields, nb );
+	updateBoundaryT( &bdElements, g, &info.lattice, &mfields );
+
+
+
+	// Sync fields
+	syncScalarField(&info.parallel, mfields.rho );
+	syncScalarField(&info.parallel, mfields.T );
+	syncPdfField(&info.parallel, mfields.U, 3 );
+	syncPdfField(&info.parallel, f, info.lattice.Q );
+	syncPdfField(&info.parallel, g, info.lattice.Q );
 	
 
 	
--- src/lbe/liModel/temperatureCollision.c
+++ src/lbe/liModel/temperatureCollision.c
@@ -2,12 +2,19 @@
 #include <liEquilibrium.h>
 #include <stdlib.h>
 #include <syncPdfField.h>
+#include <interactionForce.h>
 
 
 void temperatureCollision( struct liModelInfo* info, double* T, double* rho, double** v, int** nb, double** g ) {
 
     // Indices
-    unsigned int id, k;
+    unsigned int id, k, j;
+
+    // Dot product for correction term
+    double kappa, dot;
+
+    // Interaction force
+    double F[3];
 
     
     // Partial distributions
@@ -25,7 +32,22 @@ void temperatureCollision( struct liModelInfo* info, double* T, double* rho, dou
 	// Collide g
 	for( k = 0 ; k < info->lattice.Q ; k++ ) {
 
-	    g[id][k] = g[id][k]  -  (  g[id][k]  -  info->fields._Cv * T[id] * f_eq[k]  ) / info->fields.tau_T;
+	    // Interaction force
+	    interactionForce( info, F, rho, nb, T[id], id);
+	    
+	    // Correction term
+	    kappa = ( 1 - 0.5 / info->fields.tau_T) * info->lattice.omega[k] * info->fields._Cv * T[id] / info->lattice.cs2;
+	    
+	    dot = 0;
+	    for( j = 0 ; j < 3 ; j++) {
+		dot += info->lattice.c * info->lattice.vel[k][j] * F[j]; 
+	    }
+
+
+	    // Collision
+	    g[id][k] = g[id][k]  -
+		       (  g[id][k]  -  info->fields._Cv * T[id] * f_eq[k]  ) / info->fields.tau_T   +
+		       info->time.tstep * kappa * dot;
 
 	}
 
