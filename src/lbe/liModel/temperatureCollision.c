4284eb8416e6d1235e545e582605bdb25ced2f6c
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2016-12-02 09:30:34 -0300

    Incorporaci\'on del trabajo de compresi\'on en la colisi\'on de temperatura

--- src/lbe/liModel/temperatureCollision.c
+++ src/lbe/liModel/temperatureCollision.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <syncPdfField.h>
 #include <interactionForce.h>
+#include <p_eos.h>
 
 
 void temperatureCollision( struct liModelInfo* info, double* T, double* rho, double** v, int** nb, double** g ) {
@@ -11,7 +12,7 @@ void temperatureCollision( struct liModelInfo* info, double* T, double* rho, dou
     unsigned int id, k, j;
 
     // Dot product for correction term
-    double kappa, dot;
+    double kappa, dot, M, phi;
 
     // Interaction force
     double F[3];
@@ -32,10 +33,12 @@ void temperatureCollision( struct liModelInfo* info, double* T, double* rho, dou
 	// Collide g
 	for( k = 0 ; k < info->lattice.Q ; k++ ) {
 
+	    
 	    // Interaction force
 	    interactionForce( info, F, rho, nb, T, id);
+
 	    
-	    // Correction term
+	    // Correction term (force)
 	    kappa = ( 1 - 0.5 / info->fields.tau_T) * info->lattice.omega[k] * info->fields._Cv * T[id] / info->lattice.cs2;
 	    
 	    dot = 0;
@@ -43,12 +46,32 @@ void temperatureCollision( struct liModelInfo* info, double* T, double* rho, dou
 	    	dot += info->lattice.c * info->lattice.vel[k][j] * F[j];
 	    }
 
+	    M = kappa * dot;
 
+
+	    
+
+	    // Compresion work
+	    
+	    dot = 0;
+	    for( j = 0 ; j < 3 ; j++) {
+	    	dot += info->lattice.c * info->lattice.vel[k][j] * v[id][j];
+	    }
+
+	    phi = -T[id] * (p_eos(info, rho[id], T[id]*1.1) - p_eos(info, rho[id], T[id]*0.9)) / ( 2 * T[id]);
+
+
+	    M = M - phi * info->lattice.omega[k] * dot / (info->fields.tau_T * info->time.tstep * info->lattice.cs2);
+
+
+	    
+	    
 	    // Collision
 	    g[id][k] = g[id][k]  -
 		       (  g[id][k]  -  info->fields._Cv * T[id] * f_eq[k]  ) / info->fields.tau_T   +
-		       info->time.tstep * kappa * dot;
+		       info->time.tstep * M;
 
+	    
 	}
 
 
