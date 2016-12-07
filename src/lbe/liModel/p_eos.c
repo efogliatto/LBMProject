d1da47a1e3a97367c780795f2cd0cb5b18a4b88f
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2016-12-07 15:59:27 -0300

    Probando Peng-Robinson para boiling plate

--- src/lbe/liModel/p_eos.c
+++ src/lbe/liModel/p_eos.c
@@ -23,10 +23,10 @@ double p_eos( struct EOSInfo* info, double rho, double T ) {
 
     
     // Peng - Robinson
-    case 2:
-	alpha = 0.170151343 * info->_a / (info->_b * info->_R); 
-	beta = pow(  (1 + (0.37464+1.5422*info->_omega-0.26992*info->_omega*info->_omega)*(1-sqrt(T/alpha)))  , 2);
-	p = rho * info->_R * T / ( 1 - info->_b * rho)    -   info->_a * beta * rho * rho / (1 + 2 * info->_b * rho - info->_b * rho * info->_b * rho);
+    case 2:	
+	alpha = 0.170151343 * info->_a / (info->_b * info->_R); // Tc
+	beta = (1 + (0.37464+1.5422*info->_omega-0.26992*info->_omega*info->_omega)*(1-sqrt(T/alpha))); // sqrt(phi)
+	p = rho * info->_R * T / ( 1 - info->_b * rho)    -   info->_a * beta * beta * rho * rho / (1 + 2 * info->_b * rho - info->_b * rho * info->_b * rho);
 	break;
 
 
