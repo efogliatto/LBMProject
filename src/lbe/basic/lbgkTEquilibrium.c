146929af01d4986d3c6d59e8f775ce3806ccb04a
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2016-12-12 16:09:48 -0300

    Probando con equilibrio para g. S\'olo primer t\'ermino de U

--- src/lbe/basic/lbgkTEquilibrium.c
+++ src/lbe/basic/lbgkTEquilibrium.c
@@ -21,7 +21,7 @@ void lbgkTEquilibrium( struct latticeInfo* lattice, double rho, double v[3], dou
 
 	alpha = alpha * lattice->c;
 
-	f[k] = rho * lattice->omega[k] /* * ( 1 + alpha/lattice->cs2   +   0.5 * alpha * alpha / (lattice->cs2*lattice->cs2)  -  0.5 * beta / lattice->cs2 ) */;
+	f[k] = rho * lattice->omega[k] * ( 1 + alpha/lattice->cs2   /* +   0.5 * alpha * alpha / (lattice->cs2*lattice->cs2)  -  0.5 * beta / lattice->cs2 */ );
 	
 
     }
